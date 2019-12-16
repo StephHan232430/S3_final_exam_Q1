9914993f3e142121fb1f77cb83262ae6796a4973
Steph <blackman914037@gmail.com>
2019-12-16 11:09:18 +0800

    finished, first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# 期末考Q1
+
+學期3 A30:實作 Middleware
+
+## 功能列表
+
+- 收到 request 時，將 request 的 time-stamp、HTTP method、URL 以『 YYYY-MM-DD hh:mm:ss 』格式呈現在 terminal 中
+
+## 環境建置
+
+1. Node.js
+
+## 專案安裝流程
+
+1. 開啟 terminal，將此專案 clone 至本機
+
+```
+git clone https://github.com/StephHan232430/S3_final_exam_Q1.git
+```
+
+2. 進入專案資料夾
+
+```
+cd S3_final_exam_Q1
+```
+
+3. 安裝專案所需套件
+
+```
+npm install
+```
+
+4. 執行專案
+```
+npm run dev
+```
+
+5. 開啟網頁瀏覽器，於網址列輸入
+```
+http://localhost:3000
+```
+
+## 使用工具
+
+- [express v4.17.1](https://expressjs.com/zh-tw/)
+- [Node.js v12.13.0](https://nodejs.org/en/)
+- [Visual Studio Code v1.39.2](https://code.visualstudio.com/)
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+function reqInfo(req, res, next) {
+  let date = new Date()
+  let formatedDate = `${date.getFullYear()}-${('0' + Number(date.getMonth()) + 1).substr(-2)}-${('0' + date.getDate()).substr(-2)} ${('0' + date.getHours()).substr(-2)}:${('0' + date.getMinutes()).substr(-2)}:${('0' + date.getSeconds()).substr(-2)}`
+  console.log(`${formatedDate} | ${req.method} from ${req.url}`)
+  next()
+}
+
+app.use(reqInfo)
+
+// 列出全部 Todo
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+// 新增一筆 Todo 頁面
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+// 顯示一筆 Todo 的詳細內容
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+// 新增一筆  Todo
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.delete('/:id/delete', (req, res) => {
+  res.send('刪除 Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
